a3aca6e0e7373b5e2b0e003ed4d570f0c992ea7d
Manos Tsagkias <e.tsagkias@uva.nl>
2015-03-05 16:28:37 +0100

    Reverted to `docopt`.

--- xtas/webserver/__main__.py
+++ xtas/webserver/__main__.py
@@ -14,31 +14,28 @@ Options:
 
 from __future__ import absolute_import
 
+import logging
+
+from docopt import docopt
+
 from tornado import version as tornado_version
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-parser = argparse.ArgumentParser(description="xtas web server")
-parser.add_argument('--debug', dest='debug', action='store_true',
-                    help='Enable debugging mode.')
-parser.add_argument('--host', dest='host', default='127.0.0.1',
-                    help='Host to listen on.')
-parser.add_argument('--port', dest='port', default=5000, type=int,
-                    help='Port to listen on.')
-parser.add_argument('--threads', dest='threads', default=5, type=int,
-                    help='Number of threads.')
-args = parser.parse_args()
+app = Flask(__name__)
 
-loglevel = logging.DEBUG if args.debug else logging.INFO
+args = docopt(__doc__)
+
+loglevel = logging.DEBUG if args.get('--debug') else logging.INFO
 logging.basicConfig(level=loglevel)
 
-app.debug = args.debug
+app.debug = args.get('--debug')
 print("xtas %s REST endpoint" % __version__)
 if app.debug:
     print("Serving tasks:")
     pprint(list(taskq.tasks.keys()))
 http_server = HTTPServer(WSGIContainer(app))
-http_server.bind(args.port, address=args.host)
-http_server.start(args.threads)
+http_server.bind(args['--port'], address=args['--host'])
+http_server.start(int(args['--threads']))
 IOLoop.instance().start()
